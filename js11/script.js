855c8bcce5aa6e8e0d67fa23334787f4fc313db6
Sasha <sanykosovs@gmail.com>
2024-03-22 08:40:29 +0200

    js

--- js11/script.js
+++ js11/script.js
@@ -33,36 +33,96 @@
 //     }
 //   }
 //3
-function checkSeason() {
-  const monthInput = document.getElementById("month").value;
-  let season;
+// function checkSeason() {
+//   const monthInput = document.getElementById("month").value;
+//   let season;
 
-  switch (parseInt(monthInput)) {
-    case 1:
-    case 2:
-    case 12:
-      season = "Winter";
-      break;
-    case 3:
-    case 4:
-    case 5:
-      season = "Spring";
-      break;
-    case 6:
-    case 7:
-    case 8:
-      season = "Summer";
-      break;
-    case 9:
-    case 10:
-    case 11:
-      season = "Autumn";
-      break;
-    default:
-      season = "Invalid month";
+//   switch (parseInt(monthInput)) {
+//     case 1:
+//     case 2:
+//     case 12:
+//       season = "Winter";
+//       break;
+//     case 3:
+//     case 4:
+//     case 5:
+//       season = "Spring";
+//       break;
+//     case 6:
+//     case 7:
+//     case 8:
+//       season = "Summer";
+//       break;
+//     case 9:
+//     case 10:
+//     case 11:
+//       season = "Autumn";
+//       break;
+//     default:
+//       season = "Invalid month";
+//   }
+
+//   document.getElementById(
+//     "output"
+//   ).innerText = `The month number ${monthInput} is in the season of ${season}.`;
+//}
+//4
+// function getDays() {
+//   const monthNumber = document.getElementById("month").value;
+//   const daysInMonth = new Date(2023, monthNumber, 0).getDate();
+
+//   document.getElementById(
+//     "result"
+//   ).textContent = `Днів в цьому місяці ${monthNumber}: ${daysInMonth}`;
+// }
+//5
+// function checkColor() {
+//   const color = document.getElementById("Input").value.toLowerCase();
+//   const Message = document.getElementById("Message");
+
+//   switch (color) {
+//     case "червоний":
+//       Message.innerText = "стоп";
+//       break;
+//     case "зелений":
+//       Message.innerText = "йти";
+//       break;
+//     case "жовтий":
+//       Message.innerText = "чекати";
+//       break;
+//     default:
+//       Message.innerText =
+//         "Недійсний колір. Введіть червоний, зелений або жовтий.";
+//   }
+// }
+//6
+function calculate() {
+  const num1 = parseFloat(document.getElementById("number1").value);
+  const num2 = parseFloat(document.getElementById("number2").value);
+  const operation = document.getElementById("operation").value;
+  let result;
+
+  if (operation === "/" && num2 === 0) {
+    document.getElementById("result").textContent =
+      "Помилка на 0 ділити не можна!";
+  } else {
+    switch (operation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        result = "НЕмає такої операції";
+    }
+
+    document.getElementById("result").textContent = `Результат: ${result}`;
   }
-
-  document.getElementById(
-    "output"
-  ).innerText = `The month number ${monthInput} is in the season of ${season}.`;
 }
